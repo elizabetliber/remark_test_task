d863e488f8f04f4ab877fac4bb24fd861ba8497b
Lisa <elizabet.liber@gmail.com>
2020-11-10 23:40:13 +0700

    add hook

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
-import Image from 'next/image'
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import Image from "next/image";
+import {
+  ButtonBack,
+  ButtonNext,
+  CarouselProvider,
+  Slide,
+  Slider,
+} from "pure-react-carousel";
+import useWindowSize from "../hooks/useWindowSize";
 
 export default function Home() {
+  const { width } = useWindowSize();
   let imagesMap = [
-    {id: 1, image: "/image_1.png", title: 'Карта привилегий "Новый город"'},
-    {id: 2, image: "/image_2.png", title: 'Бесплатное такси в офис продаж Нового города'},
-    {id: 3, image: "/image_3.png", title: 'Квартиры с отделкой от застройщика'},
-  ]
-  
-  
+    { id: 1, image: "/image_1.png", title: 'Карта привилегий "Новый город"' },
+    {
+      id: 2,
+      image: "/image_2.png",
+      title: "Бесплатное такси в офис продаж Нового города",
+    },
+    {
+      id: 3,
+      image: "/image_3.png",
+      title: "Квартиры с отделкой от застройщика",
+    },
+  ];
+
   return (
     <div className="section">
       <div className="section__header">
@@ -18,23 +33,28 @@ export default function Home() {
             naturalSlideWidth={100}
             naturalSlideHeight={125}
             totalSlides={3}
-            visibleSlides={3}
+            visibleSlides={width <= 320 ? 1 : 3}
           >
             <Slider>
-          {imagesMap.map((el, index) => (
-            <Slide index={index}>
-            <div className="section__header-card">
-              <Image className="section__header-image" src={el.image} width={848} height={424} />
-              <div className="section__header-image-title">
-                {el.title}
-              </div>
-            </div>
-            </Slide>
-          ))}
+              {imagesMap.map((el, index) => (
+                <Slide index={index}>
+                  <div className="section__header-card">
+                    <Image
+                      className="section__header-image"
+                      src={el.image}
+                      width={848}
+                      height={424}
+                    />
+                    <div className="section__header-image-title">
+                      {el.title}
+                    </div>
+                  </div>
+                </Slide>
+              ))}
             </Slider>
-                <ButtonBack>Back</ButtonBack>
-                <ButtonNext>Next</ButtonNext>
-              </CarouselProvider>
+            <ButtonBack>Back</ButtonBack>
+            <ButtonNext>Next</ButtonNext>
+          </CarouselProvider>
         </div>
         <div className="section__header-more">
           <a>Узнать больше</a>
@@ -42,19 +62,30 @@ export default function Home() {
       </div>
       <div className="section__body">
         <div className="section__body-images">
-          <Image className="section__body-image" src="/image_4.png" alt="" width={993} height={324} />
+          <Image
+            className="section__body-image"
+            src="/image_4.png"
+            alt=""
+            width={993}
+            height={324}
+          />
         </div>
         <div className="news">
-          <div className="news__item-title">Просторные квартиры с гардеробными </div>
+          <div className="news__item-title">
+            Просторные квартиры с гардеробными{" "}
+          </div>
           <div className="news__item">
-            Жилой комплекс "Новый город" расположен в одном из самых развивающихся районов
-            Обнинск - Заовражье. Несмотря на близость к центру ( 5 минут езды до ТРК "Триумф Плаза"),
-            место здесь тихое и спокойное
+            Жилой комплекс "Новый город" расположен в одном из самых
+            развивающихся районов Обнинск - Заовражье. Несмотря на близость к
+            центру ( 5 минут езды до ТРК "Триумф Плаза"), место здесь тихое и
+            спокойное
           </div>
           <div className="news__item-date">
-            Архитектура и дизайн комплекса - яркий пример удачного сочетания классических традиций
-            строительства с современными материалами и технологиями. Территория микрорайона
-            разделена на ряд благоустроенных зон с детскимии игровыми площадками, спортивными тренажерами и прогулочными дорожками
+            Архитектура и дизайн комплекса - яркий пример удачного сочетания
+            классических традиций строительства с современными материалами и
+            технологиями. Территория микрорайона разделена на ряд
+            благоустроенных зон с детскимии игровыми площадками, спортивными
+            тренажерами и прогулочными дорожками
           </div>
         </div>
       </div>
